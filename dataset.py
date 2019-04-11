fce598030aa7be6af90f72c923895bfc608c95c5
Oliver Hill <oliverhi@umich.edu>
2019-04-11 11:00:16 -0400

    setting up network pipeline

--- dataset.py
+++ dataset.py
@@ -81,14 +81,10 @@ def aqindex(indexrange: str, concenrange: str, concentration: float) -> int:
     Ilow, Ihigh = map(lambda x: int(x), indexrange.split('-'))
     Clow, Chigh = map(lambda x: float(x), concenrange.split('-'))
 
-    return ((Ihigh - Ilow)/(Chigh - Clow)) * (concentration - Clow) + Ilow
+    return round(((Ihigh - Ilow)/(Chigh - Clow)) * (concentration - Clow) + Ilow)
 
 
 
-
-df1 = pd.read_csv('daily_06_067_0010_2018.csv')
-df2 = pd.read_csv('daily_06_067_0015_2018.csv')
-df3 = pd.read_csv('aqidaily.csv')
 
 
 first_parameters = [
@@ -105,68 +101,173 @@ second_parameters = [
     'Outdoor Temperature',
 ]
 
-days = [ ]
 
-for param in first_parameters:
-    days.append(set(df1.loc[df1['Parameter Name'] == param]['Day In Year (Local)'].unique()))
+def build_df(df1, df2, df3):
 
-for param in second_parameters:
-    days.append(set(df2.loc[df2['Parameter Name'] == param]['Day In Year (Local)'].unique()))
+    days = [ ]
+
+    for param in first_parameters:
+        days.append(set(df1.loc[df1['Parameter Name'] == param]['Day In Year (Local)'].unique()))
+
+    for param in second_parameters:
+        days.append(set(df2.loc[df2['Parameter Name'] == param]['Day In Year (Local)'].unique()))
 
 
-intersection = days[0].intersection(*days[1:])
+    intersection = days[0].intersection(*days[1:])
 
-new_df = pd.DataFrame(columns = [
-    'Day Number',
-    'Date',
-    'PM10',
-    'O3',
-    'NO2',
-    'CO',
-    'PM10 AQI',
-    'O3 AQI',
-    'NO2 AQI',
-    'CO AQI',
-    'Wind Speed',
-    'Wind Direction',
-    'Temperature'
-])
+    # Ignore leap years.
+    intersection.discard(366)
 
-# Start building the new dataframe.
-for day in intersection:
+    print(f'Days in intersection: {len(intersection)}')
 
-    # Dataframe of just this days measurements.
-    measure = df1.loc[df1['Day In Year (Local)'] == day]
+    new_df = pd.DataFrame(columns = [
+        'Day Number',
+        'Date',
+        'PM10',
+        'O3',
+        'NO2',
+        'CO',
+        'PM10 AQI',
+        'O3 AQI',
+        'NO2 AQI',
+        'CO AQI',
+        'Wind Speed',
+        'Wind Direction',
+        'Temperature'
+    ])
 
-    pm10 = measure.loc[measure['Parameter Name'] == 'PM10 - LC']
-    o3 = measure.loc[measure['Parameter Name'] == 'Ozone']
-    no2 = measure.loc[measure['Parameter Name'] == 'Nitrogen dioxide (NO2)']
-    windspeed = measure.loc[measure['Parameter Name'] == 'Wind Speed - Resultant']
-    windir = measure.loc[measure['Parameter Name'] == 'Wind Direction - Resultant']
+    # Start building the new dataframe.
+    for day in intersection:
 
-    # Second dataframe.
-    measure = df2.loc[df2['Day In Year (Local)'] == day]
+        # Dataframe of just this days measurements.
+        measure = df1.loc[df1['Day In Year (Local)'] == day]
 
-    co = measure.loc[measure['Parameter Name'] == 'Carbon monoxide']
-    temp = measure.loc[measure['Parameter Name'] == 'Outdoor Temperature']
+        pm10 = measure.loc[measure['Parameter Name'] == 'PM10 - LC']
+        o3 = measure.loc[measure['Parameter Name'] == 'Ozone']
+        no2 = measure.loc[measure['Parameter Name'] == 'Nitrogen dioxide (NO2)']
+        windspeed = measure.loc[measure['Parameter Name'] == 'Wind Speed - Resultant']
+        windir = measure.loc[measure['Parameter Name'] == 'Wind Direction - Resultant']
 
-    from ipdb import set_trace as debug; debug()
+        # Second dataframe.
+        measure = df2.loc[df2['Day In Year (Local)'] == day]
 
-    new_df = new_df.append({
-        'Day Number': day,
-        'Date': pm10['Date (Local)'].values[0],
-        'PM10': pm10['Arithmetic Mean'].values[0],
-        'O3': o3['Arithmetic Mean'].values[0],
-        'NO2': no2['Arithmetic Mean'].values[0],
-        'CO': co['Arithmetic Mean'].values[0],
-        'PM10 AQI': df3.loc[day - 1]['PM10'],
-        'O3 AQI': df3.loc[day - 1]['Ozone'],
-        'NO2 AQI': df3.loc[day - 1]['NO2'],
-        'CO AQI': df3.loc[day - 1]['CO'],
-        'Wind Speed': windspeed['Arithmetic Mean'].values[0],
-        'Wind Direction': windir['Arithmetic Mean'].values[0],
-        'Temperature': temp['Arithmetic Mean'].values[0]
-    }, ignore_index = True)
+        co = measure.loc[measure['Parameter Name'] == 'Carbon monoxide']
+        temp = measure.loc[measure['Parameter Name'] == 'Outdoor Temperature']
+
+        new_df = new_df.append({
+            'Day Number': day,
+            'Date': pm10['Date (Local)'].values[0],
+            'PM10': pm10['Arithmetic Mean'].values[0],
+            'O3': o3['Arithmetic Mean'].values[0],
+            'NO2': no2['Arithmetic Mean'].values[0],
+            'CO': co['Arithmetic Mean'].values[0],
+            'PM10 AQI': df3.loc[day - 1]['PM10'],
+            'O3 AQI': df3.loc[day - 1]['Ozone'],
+            'NO2 AQI': df3.loc[day - 1]['NO2'],
+            'CO AQI': df3.loc[day - 1]['CO'],
+            'Wind Speed': windspeed['Arithmetic Mean'].values[0],
+            'Wind Direction': windir['Arithmetic Mean'].values[0],
+            'Temperature': temp['Arithmetic Mean'].values[0]
+        }, ignore_index = True)
+
+    return new_df
+
+
+
+def plots():
+    from matplotlib import pyplot as plt
+    import seaborn as sns
+    import matplotlib as mpl
+    mpl.style.use('seaborn')
+
+    plt.plot(range(new_df['CO AQI'].values.shape[0]), new_df['CO AQI'].values,
+            label = 'Reported AQI')
+
+    line = [ ]
+    for concentration in new_df['CO'].values:
+        crange = in_range(AQI_table['CO'], concentration)
+        line.append(aqindex(
+            AQI_table['Index'][crange],
+            AQI_table['CO'][crange],
+            concentration))
+
+    plt.plot(range(len(line)), np.array(line), label='Estimated AQI')
+    plt.legend(loc='best')
+    plt.xlabel('Day in Year (2018)')
+    plt.ylabel('Carbon Monoxide AQI')
+    plt.title('Reported CO AQI vs. Calculated CO AQI')
+    plt.show()
+    # print(np.mean(new_df['CO AQI'].values - np.array(line)))
+
+
+def get_dfs(year: int) -> tuple:
+    df1 = pd.read_csv(f'data/daily_06_067_0010_{year}.csv')
+    df2 = pd.read_csv(f'data/daily_06_067_0015_{year}.csv')
+    df3 = pd.read_csv(f'data/aqidaily{year}.csv')
+
+    return df1, df2, df3
+
+
+def build_network_data():
+    '''Testing on 2017 CO for now.'''
+
+    df2015 = build_df(*get_dfs(2015))
+    df2016 = build_df(*get_dfs(2016))
+    df2017 = build_df(*get_dfs(2017))
+    df2018 = build_df(*get_dfs(2018))
+
+    n_rows = df2015.shape[0] + df2016.shape[0] + df2018.shape[0] - 3
+    X = np.zeros((n_rows, 7), dtype=float)
+    y = np.zeros(n_rows, dtype=int)
+
+    row = 0
+
+    for df in [df2015, df2016, df2018]:
+        print(row, df.shape[0] - 1, row + df.shape[0] - 1)
+        X[row : row + df.shape[0] - 1, :-1] = df[['Day Number', 'CO', 'CO AQI', 'Wind Speed', 'Wind Direction', 'Temperature']].values[:-1, :]
+        X[row : row + df.shape[0] - 1, -1] = np.squeeze(df[['Day Number']].values[1:])
+        y[row : row + df.shape[0] - 1] = np.squeeze(df[['CO AQI']].values[1:])
+
+        row += df.shape[0] - 1
+
+    Xtest = np.zeros((df2017.shape[0] - 1, 7), dtype=float)
+    Xtest[:, :-1] = df2017[['Day Number', 'CO', 'CO AQI', 'Wind Speed', 'Wind Direction', 'Temperature']].values[:-1, :]
+    Xtest[:, -1] = np.squeeze(df2017[['Day Number']].values[1:])
+
+    ytest = np.zeros(df2017.shape[0], dtype=int)
+    y = np.squeeze(df2017[['CO AQI']].values[1:])
+
+
+    return X, Xtest, y, ytest
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    print('hello!')
+    # df1 = pd.read_csv('data/daily_06_067_0010_2018.csv')
+    # df2 = pd.read_csv('data/daily_06_067_0015_2018.csv')
+    # df3 = pd.read_csv('data/aqidaily2018.csv')
+
+    # df = build_df(df1, df2, df3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
